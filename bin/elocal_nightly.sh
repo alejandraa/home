d7c0058e5299a48a6ffd4d956589e3ebe1a0cb62
Tom Scott <tom.scott@elocal.com>
2012-10-09 10:51:06 -0400

    Add confirmation email to nightly script, test with bash --login

--- bin/elocal_nightly.sh
+++ bin/elocal_nightly.sh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 #
-# Download the latest database from stage.elocal.com and
-# index the new data with Solr.
+# Nightly database import script for eLocal.com.
 
+# Download the latest database from our backup server
 cd /Users/tom/Code/elocal/
-bundle exec thor db:import:stage
-bundle exec rake db:migrate
-bundle exec thor solr:index_accounts
-bundle exec thor solr:index_categories
-bundle exec thor solr:index_profiles
-bundle exec rake email:nightly_db_pull_notification
+bundle exec thor db:import:production
+
+# Index the new data with Solr
+#bundle exec thor solr:index_accounts
+#bundle exec thor solr:index_profiles
+#bundle exec thor solr:index_categories
+
+# Send a confirmation email when everything is done
+EMAIL="tubbo@psychedeli.ca"
+SUBJECT="Imported the production database to playa."
+MESSAGE="db:import:production has been run, and the database has been delivered to your local development machine."
+echo "To: $EMAIL\nSubject:$SUBJECT\nContent-Type:text/plain;charset="us-ascii"\n\n$MESSAGE" | sendmail -t
