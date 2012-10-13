b89a713d44762b9ac4db8ec5de4f7a91074343b3
Tom Scott <tubbo@psychedeli.ca>
2012-10-13 17:50:56 -0400

    Update example file

--- config/example.aws.zsh
+++ config/example.aws.zsh
@@ -1,5 +1,13 @@
 # Put your AWS credentials here so they can be picked up by EC2 API/AMI
 # tools.
 
+# For AWS command-line tools
 export AWS_ACCESS_KEY=""
 export AWS_SECRET_KEY=""
+
+# For AWS SDK
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+export KNIFE_USER_NAME=''
+export AWS_SSH_KEY_ID=''
+
