bcf8ecc26b21f3fdb045ea5fc919ec3314ede2ce
Tom Scott <tubbo@psychedeli.ca>
2012-10-16 11:45:41 -0400

    Use vendored, submoduled Antigen
    - Amend reload() to also update all plugins

--- lib/dots/plugins.zsh
+++ lib/dots/plugins.zsh
@@ -1,18 +1,26 @@
 # Load the plugin architecture
-source "$ZSH/vendor/antigen.zsh"
+source "$ZSH/vendor/antigen/antigen.zsh"
 
 # Set up the plugin architecture
 antigen-lib
 
-# Load plugins
-antigen-bundle tubbo/dots lib/plugins/aws
-antigen-bundle tubbo/dots lib/plugins/bundler
+# Bare Oh My ZSH plugins
 antigen-bundle cap
-antigen-bundle tubbo/dots lib/plugins/git
-antigen-bundle tubbo/dots lib/plugins/git-process
-antigen-bundle tubbo/dots lib/plugins/knife
-antigen-bundle tubbo/dots lib/plugins/macvim
-antigen-bundle tubbo/dots lib/plugins/osx
-antigen-bundle tubbo/dots lib/plugins/rails3
-antigen-bundle tubbo/dots lib/plugins/ruby
+
+# Local modifications to OMZ plugins
+antigen-bundle $HOME/.dots/lib/plugins/aws
+antigen-bundle $HOME/.dots/lib/plugins/bundler
+antigen-bundle $HOME/.dots/lib/plugins/git
+antigen-bundle $HOME/.dots/lib/plugins/git-process
+antigen-bundle $HOME/.dots/lib/plugins/knife
+antigen-bundle $HOME/.dots/lib/plugins/macvim
+antigen-bundle $HOME/.dots/lib/plugins/osx
+antigen-bundle $HOME/.dots/lib/plugins/rails3
+antigen-bundle $HOME/.dots/lib/plugins/ruby
+
+# From alternative sources
 antigen-bundle zsh-users/zsh-syntax-highlighting
+antigen-bundle kennethreitz/autoenv
+
+# Apply loaded plugins to the environment
+antigen-apply
