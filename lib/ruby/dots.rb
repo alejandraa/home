85872043be9d5de576f227cd76042d3452680c1e
Tom Scott <tubbo@psychedeli.ca>
2012-11-15 20:51:48 -0500

    Add Dots.root

--- lib/ruby/dots.rb
+++ lib/ruby/dots.rb
@@ -1,9 +1,18 @@
 require 'rubygems'
 require 'fileutils'
 require 'bundler'
-Bundler.setup
+Bundler.require :default
 require 'dots/dot_file'
 require 'dots/command'
 require 'dots/version'
 
-module Dots; end
+module Dots
+  # The root path of the DOTS gem.
+  def self.root
+    @root_dir ||= begin
+      spec = Gem::Specification.find_by_name 'zsh_dots'
+      spec.gem_dir
+    end
+  end
+end
+
