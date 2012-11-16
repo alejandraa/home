c51037f7d15f44e82fc73bd2700c18dce7857bf6
Tom Scott <tubbo@psychedeli.ca>
2012-11-15 20:11:36 -0500

    Clean up the Command class and add useful aliases.

--- lib/ruby/dots/command.rb
+++ lib/ruby/dots/command.rb
@@ -1,7 +1,12 @@
+require 'dots/persistence'
+
 module Dots
   class Command < Thor
     include FileUtils
     include Thor::Actions
+
+    include Dots::Persistence
+    #include Dots::Installer
 
     default_task :usage
 
@@ -37,55 +42,9 @@ The following tasks are meant to help you use the shell more efficiently...
       run "source ~/.dots/vendor/antigen/antigen.zsh && antigen-update"
     end
 
-    #curl -L https://get.rvm.io | bash -s stable --ruby=rbx --gems=rails,puma
-
-    desc :link, "Symlink your ~/.dots/config into dotfiles"
-    def link
-      Dir["config/*"].each do |config_file|
-        unless File.directory? config_file or config_file =~ /\.zsh$/
-          config_file.gsub! /config\/|.example/, ""
-          config_file_path = File.expand_path "~/.dots/config/#{config_file}"
-          dot_file_path = File.expand_path "~/.#{config_file}"
-
-          if File.exists? dot_file_path
-            say "Did not symlink #{config_file} since one already exists"
-          else
-            File.symlink config_file_path, dot_file_path
-            say "Symlinked ~/.#{config_file}"
-          end
-        end
-      end
-
-      update
-    end
-
     desc :version, "Show the current version of DOTS"
     def version
       say "DOTS version #{Dots::VERSION} - http://tubbo.github.com/dots"
-    end
-
-    desc :persist, "Copy a dotfile to .dots/config and symlink the original location"
-    def persist file_name
-      dot_file = Dots::DotFile.new file_name
-
-      if dot_file.save
-        say "#{dot_file} saved to DOTS!"
-      else
-        say "Error: #{dot_file} could not be symlinked:"
-        dot_file.errors.full_messages.each { |msg| say "- #{msg}" }
-      end
-    end
-
-    desc :forget, "Remove the symlink and restore a dotfile back to its original location"
-    def forget file_name
-      dot_file = Dots::DotFile.find file_name
-
-      if dot_file.destroy
-        say "#{dot_file} is no longer being persisted."
-      else
-        say "Error: #{dot_file} could not be forgotten:"
-        dot_file.errors.full_messages.each { |msg| say "- #{msg}" }
-      end
     end
   end
 end
---
+++ lib/ruby/dots/installer.rb
@@ -0,0 +1,2 @@
+module Installer
+end
