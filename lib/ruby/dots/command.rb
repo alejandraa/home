da32675b0902e4f1de79b7f8089d7c135c4a256f
Tom Scott <tubbo@psychedeli.ca>
2012-11-15 21:13:11 -0500

    Break out installation tasks and refactor Dots::Command to just be documentation, basically

--- lib/ruby/dots/command.rb
+++ lib/ruby/dots/command.rb
@@ -2,48 +2,45 @@ require 'dots/persistence'
 
 module Dots
   class Command < Thor
-    include FileUtils
     include Thor::Actions
-
     include Dots::Persistence
+    include Dots::Installation
 
     default_task :usage
 
     desc :usage, "Show usage information"
     def usage
-      say <<-TEXT
-
-The DOTS Project
-
-DOTS is a ZSH Framework for managing your dotfiles and other shell configuration.
-It also gives you some nice, sensible defaults and time-saver aliases to better
-work with and understand your shell environment.
-
-The following tasks are meant to help you use the shell more efficiently...
-
-      TEXT
-
-      help
+      say Dots::USAGE_INFORMATION and help
     end
 
+    desc :install, "Copies DOTS to your home directory."
     def install
-      unless File.exists? File.expand_path("~/.dots")
-        run "git clone https://github.com/tubbo/dots.git ~/.dots" unless File.exists?
-      end
-
-      invoke :link
+      copy_to_home_directory! and link_dot_files!
     end
 
     desc :update, "Update DOTS, Antigen and all plugins to their latest version."
     def update
-      run "cd ~/.dots && git pull origin master"
-      run "cd ~/.dots && git submodule foreach 'git pull origin master' && git submodule update"
-      run "source ~/.dots/vendor/antigen/antigen.zsh && antigen-update"
+      pull_from_origin! and run 'reload'
     end
 
     desc :version, "Show the current version of DOTS"
     def version
       say "DOTS version #{Dots::VERSION} - http://tubbo.github.com/dots"
+    end
+
+    desc :link, "Symlink your ~/.dots/config into dotfiles"
+    def link
+      link_dot_files!
+    end
+
+    desc :persist, "Copy a dotfile to .dots/config and symlink the original location"
+    def persist file_name
+      persist_dot_file! file_name
+    end
+
+    desc :forget, "Remove the symlink and restore a dotfile back to its original location"
+    def forget file_name
+      forget_dot_file! file_name
     end
   end
 end
