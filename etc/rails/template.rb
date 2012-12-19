12eaa9fe6cd83d42720df2bf52e2bc510759b373
Tom Scott <tubbo@psychedeli.ca>
2012-12-18 23:09:42 -0500

    Refactor Rails template.

--- etc/rails/template.rb
+++ etc/rails/template.rb
@@ -2,25 +2,58 @@
 # Default Rails application template
 #
 
-# PostgreSQL for the database, set up this app's user and the DB
-database = YAML::load_file("#{Rails.root}/config/database.yml")['development'].symbolize_keys
-sh "createuser -s #{database[:user]}"
+gem 'haml'
+gem 'puma'
+
+gem_group :development do
+  gem 'haml-rails'
+  gem 'ruby_parser'
+  gem 'hpricot'
+end
+
+gem_group :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+end
+
+initializer "generators.rb", <<-RUBY
+Rails.application.config.generators do |g|
+  g.template_engine :haml
+  g.test_framework :rspec, fixtures: true, fixture_location: "spec/fixtures"
+end
+RUBY
+
+rakefile "base.rake" do
+  <<-RUBY
+    require 'rspec/core/rake_task'
+    RSpec::Core::RakeTask.new :spec
+
+    task :test => ['db:test:prepare', 'spec']
+
+    task :server do
+      sh "rails server puma"
+    end
+  RUBY
+end
+
+file ".env" do
+  <<-RUBY
+    export GEM_HOME='vendor/gems'
+  RUBY
+end
+
+file "README.md" do
+  <<-MARKDOWN
+    # #{app_name.titleize}
+
+    A thorough description of the application.
+  MARKDOWN
+end
+
+run "createuser -s #{app_name}"
+run "bundle install"
+run "bundle exec rails generate rspec:install"
 rake 'db:create'
 rake 'db:test:prepare'
-
-# Haml for HTML markup
-gem 'haml'
-gem 'haml-rails', group: :development
-gem 'ruby-parser', group: :development, require: false
-gem 'hpricot', group: :development, require: false
-
-# RSpec and Capybara for testing
-gem 'rspec-rails', group: :test
-gem 'capybara', group: :test
-
-# TODO: Write application config for generators
-
-# Set up a repository
-git :init
-git add: '.'
-git commit: "-a -m 'Initial commit'"
+run "rm -rf test/; rm -rf README.rdoc; rm -rf public/index.html"
+run "git init && git add . && git commit -am 'Initial commit.'"
