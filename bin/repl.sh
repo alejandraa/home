50cbe7fa96ad10b04e528b90e7f782398ac818c1
Tom Scott <tubbo@psychedeli.ca>
2013-01-01 22:57:46 -0500

    Remove stuff from bin we don't need.

--- bin/repl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-for file in $(grep -il "codehilite" vendor/assets/stylesheets/pygments/*.css); do
-  sed -e "s/codehilite/highlight/g" $file > tmp/current.tmp
-  mv tmp/current.tmp $file
-done
