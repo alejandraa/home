2fde2387f03f1e18afa6c4bd4de34a5d5bb25cac
Tom Scott <tubbo@psychedeli.ca>
2013-01-04 09:44:13 -0500

    Add fn for wrapping global gems
    This is kind of a hacky solution to an ongoing Ruby problem. When
    you want to use a gem's binary in any directory, regardless of
    what GEM_HOME is set to, you can wrap the command with `globalize`
    to execute that command in the context of the $GLOBAL_GEM_HOME,
    as set in ~/.env. This is similar to how `bundle exec` works, by
    executing the succeeding command in the context of the isolated
    GEM_HOME. However, in the event that we have GEM_HOME overridden
    in a local .env, `globalize` can ostensibly reverse this tactic
    and execute the gem in the global gemset.

--- lib/dots/functions.zsh
+++ lib/dots/functions.zsh
@@ -126,3 +126,8 @@ gg_replace() {
 gg_dasherize() {
   gg_replace $1 `echo $1 | sed -e 's/_/-/g'` $2
 }
+
+# Wrap a Ruby binary in the global GEM_HOME.
+function globalize() {
+  GEM_HOME=$GLOBAL_GEM_HOME $1
+}
