acd6910c8b1db8b7b57ab7e00e7be8ecee925e43
Tom Scott <tubbo@psychedeli.ca>
2013-01-15 22:20:46 -0500

    Add Heroku plugin

---
+++ lib/plugins/heroku/heroku.plugin.zsh
@@ -0,0 +1,12 @@
+# Heroku plugin
+#
+# A tool for managing continuous deployment from Travis to Heroku.
+
+# Load Heroku config
+source ~/.dots/config/heroku.zsh
+
+# Secure your API key on Travis, so it can be uploaded to CI and
+# used to deploy your Heroku app.
+function secure_api_key() {
+  travis encrypt $1 HEROKU_API_KEY=$HEROKU_API_KEY
+}
