49b4cc3e01c0ebae228d29f4015221c526a87561
Tom Scott <tubbo@psychedeli.ca>
2013-01-29 09:06:44 -0500

    Reorganize Git aliases

--- lib/plugins/git/git.plugin.zsh
+++ lib/plugins/git/git.plugin.zsh
@@ -1,35 +1,28 @@
 # Aliases
 alias g='git'
 compdef g=git
-alias gst='git status'
-compdef _git gst=git-status
+
+# Branching
 alias gu='git pull'
 compdef _git gl=git-pull
 alias gur='git pull --rebase'
 compdef _git gl=git-pull
-alias gup='git fetch && git rebase'
-compdef _git gup=git-fetch
 alias gp='git push'
 compdef _git gp=git-push
 alias gpf='git push tubbo'
 compdef _git gpf=git-push
 gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
-alias gc='git commit -v'
-compdef _git gc=git-commit
-alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
-alias gcm='git checkout master'
 alias gb='git branch'
 compdef _git gb=git-branch
 alias gba='git branch -a'
 compdef _git gba=git-branch
-alias gcount='git shortlog -sn'
-compdef gcount=git
-alias gcp='git cherry-pick'
-compdef _git gcp=git-cherry-pick
+alias gm='git merge'
+compdef _git gm=git-merge
+
+# Logging and Viewing
 alias gl='git l'
 compdef _git glg=git-log
 alias gls='git log --stat --max-count=5'
@@ -38,22 +31,28 @@ alias glg='git log --graph --max-count=5'
 compdef _git glg=git-log
 alias lol='git lol'
 compdef _git lol=git-log
+alias gcount='git shortlog -sn'
+compdef gcount=git
+alias gst='git status'
+compdef _git gst=git-status
 alias gsts='git status -s'
 compdef _git gss=git-status
-alias ga='git add'
-compdef _git ga=git-add
-alias gm='git merge'
-compdef _git gm=git-merge
-alias gls="git l"
-compdef _git gls=git-log
-alias grv='git remote -v'
-compdef _get grv=git-remote
 alias gd='git diff'
 compdef _get grv=git-diff
 alias gg='git grep'
 compdef _get gg=git-grep
 alias gbl='git blame'
 compdef _get gbl=git-blame
+
+# Manipulation
+alias gcp='git cherry-pick'
+compdef _git gcp=git-cherry-pick
+alias ga='git add'
+compdef _git ga=git-add
+alias gls="git l"
+compdef _git gls=git-log
+alias grv='git remote -v'
+compdef _get grv=git-remote
 alias gt='git reset'
 compdef _get gt=git-reset
 alias gth='git reset --hard'
@@ -62,6 +61,10 @@ alias gts='git reset --soft'
 compdef _get gt=git-reset
 alias gthh='git reset --hard HEAD'
 compdef _get gt=git-reset
+alias gc='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
 
 # Stash
 alias gs='git stash save'
@@ -69,7 +72,7 @@ compdef _get gsh=git-stash
 alias gsp='git stash pop'
 compdef _get gsh=git-stash
 
-# Merging and Rebasing
+# Rebase
 alias gr='git rebase'
 compdef _get gg=git-rebase
 alias grc='git rebase --continue'
