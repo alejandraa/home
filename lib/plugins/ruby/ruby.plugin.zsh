c9dca2a78abbe59e961594956e26c9f886875291
Tom Scott <tubbo@psychedeli.ca>
2013-04-22 09:37:31 -0400

    Refactor Ruby singleton testing
    I'm going to write a binary replacement for this that works well with
    vim-vroom and running "nearest" tests to a given line number. But for
    now, Rob's functions will work fine in the new name `rbtest`. Our binary
    will be called `rtest` (eventually).

--- lib/plugins/ruby/ruby.plugin.zsh
+++ lib/plugins/ruby/ruby.plugin.zsh
@@ -5,12 +5,26 @@ alias sgem='sudo gem'
 # Find ruby file
 alias rfind='find . -name "*.rb" | xargs grep -n'
 
-## @robdimarco <http://innovationontherun.com> contributed the following code: ##
+# Run a single Ruby test using `ruby -Itest`. This is the built-in testing
+# framework that ships with Ruby 1.9 and is useful for running quick tests
+# that don't require database connectivity or awareness of the Rails app.
+#
+# Thanks @robdimarco
+rbtest() {
+  if [ -f $1 ]; then
+    bundle exec ruby -Itest $@;
+  else
+    echo "Please specify a file(s) to test"
+  fi;
+}
+
 
 # Run a single Ruby test using Rake. This will also migrate the database and
 # generally prepare the environment for testing, and is useful for tests
 # which require a specific database setup to function.
-_rbtest_with_rake() {
+#
+# Thanks @robdimarco
+rbtest_with_rake() {
   if [ -f $1 ]; then
     case `echo $1 | cut -f 2 -d '/'` in
       unit)
@@ -31,22 +45,3 @@ _rbtest_with_rake() {
     bundle exec rake test
   fi
 }
-eval "function rtest_rake () { _rbtest_with_rake \$@}"
-
-# Run a single Ruby test using `ruby -Itest`. This is the built-in testing
-# framework that ships with Ruby 1.9 and is useful for running quick tests
-# that don't require database connectivity or awareness of the Rails app.
-_rbtest_without_rake() {
-  if [ -f $1 ]; then
-    bundle exec ruby -Itest $@;
-  else
-    echo "Please specify a file(s) to test"
-  fi;
-}
-eval "function rtest_bare () {_rbtest_without_rake \$@}"
-
-## end @robdimarco contributions ##
-
-# Disable autocorrect
-alias rb_test='nocorrect rtest_bare'
-alias rb_test_rake='nocorrect rtest_rake'
