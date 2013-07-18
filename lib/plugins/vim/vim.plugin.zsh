881f8d065a90ac74410b3615cc9b6481d2af3c7e
Tom Scott <tubbo@psychedeli.ca>
2013-07-18 10:59:23 -0400

    Make vundle into a function

--- lib/plugins/vim/vim.plugin.zsh
+++ lib/plugins/vim/vim.plugin.zsh
@@ -1,3 +1,8 @@
-# Add alias for installing Vundle plugins
-alias vundle='vim +BundleInstall +qall'
+# Install all Vim plugins with Vundle
+vundle() {
+  vim +BundleInstall +qall
+  echo "Your Vundle has been installed to ~/.vim/bundle"
+}
+
+# Define an easy-to-remember alias for the vundle command
 alias vimb=vundle
