b2c2154e1c6e42fd46ed6227c4725a28d2aeb41a
Tom Scott <tubbo@psychedeli.ca>
2013-07-25 14:59:28 -0400

    Refactor plugin bundle file

--- lib/dots/plugins.zsh
+++ lib/dots/plugins.zsh
@@ -5,23 +5,23 @@ source "$ZSH/vendor/antigen/antigen.zsh"
 antigen-lib
 
 # Local plugins and OMZ overrides
-antigen-bundle $HOME/.dots lib/plugins/bundler
-antigen-bundle $HOME/.dots lib/plugins/git
-antigen-bundle $HOME/.dots lib/plugins/knife
-antigen-bundle $HOME/.dots lib/plugins/ruby-rails
-antigen-bundle $HOME/.dots lib/plugins/bower
-antigen-bundle $HOME/.dots lib/plugins/heroku
-antigen-bundle $HOME/.dots lib/plugins/vim
+antigen bundle tubbo/dots lib/plugins/bundler
+antigen bundle tubbo/dots lib/plugins/git
+antigen bundle tubbo/dots lib/plugins/knife
+antigen bundle tubbo/dots lib/plugins/ruby-rails
+antigen bundle tubbo/dots lib/plugins/bower
+antigen bundle tubbo/dots lib/plugins/heroku
+antigen bundle tubbo/dots lib/plugins/vim
 
 # Plugins from alternative sources
-antigen-bundle zsh-users/zsh-syntax-highlighting
-antigen-bundle kennethreitz/autoenv
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle kennethreitz/autoenv
 
 # Plugins directly from OMZ
-antigen-use oh-my-zsh
+antigen use oh-my-zsh
 antigen bundle osx
 antigen bundle vi-mode
 antigen bundle tmux
 
 # Apply loaded plugins to the environment
-antigen-apply
+antigen apply
