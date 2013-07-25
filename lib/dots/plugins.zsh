d6afa77d36f556fb81958d8cddff2328c1512bd2
Tom Scott <tubbo@psychedeli.ca>
2013-07-25 11:04:59 -0400

    Update Antigen API and use Tmuxinator plugin

--- lib/dots/plugins.zsh
+++ lib/dots/plugins.zsh
@@ -12,15 +12,16 @@ antigen-bundle $HOME/.dots lib/plugins/ruby-rails
 antigen-bundle $HOME/.dots lib/plugins/bower
 antigen-bundle $HOME/.dots lib/plugins/heroku
 antigen-bundle $HOME/.dots lib/plugins/vim
+antigen-bundle $HOME/.dots lib/plugins/tmuxinator
 
 # Plugins from alternative sources
 antigen-bundle zsh-users/zsh-syntax-highlighting
 antigen-bundle kennethreitz/autoenv
 
 # Plugins directly from OMZ
-antigen-bundle osx
-antigen-bundle vi-mode
-antigen-bundle tmux
+antigen-use oh-my-zsh osx
+antigen-use oh-my-zsh vi-mode
+antigen-use oh-my-zsh tmux
 
 # Apply loaded plugins to the environment
 antigen-apply
