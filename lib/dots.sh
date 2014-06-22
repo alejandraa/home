5eee63bc23c739b0d193e514abd63a09c7a03361
Tom Scott <tscott@telvue.com>
2014-06-21 23:18:22 -0400

    Prepend path with dots path

--- lib/dots.sh
+++ lib/dots.sh
@@ -1,11 +1,18 @@
 # add a function path
 fpath=($ZSH/lib/dots/functions $ZSH/lib/dots/completion $fpath)
 
+# static binary path
+export DOTS_PATH=$HOME/.dots/bin
+
 # Load and run compinit
 autoload -U compinit
 compinit -i
 
+# Load DOTS source code
 source "$ZSH/lib/dots/functions.zsh"
 source "$ZSH/lib/dots/aliases.zsh"
 source "$ZSH/lib/dots/directories.zsh"
 source "$ZSH/lib/dots/plugins.zsh"
+
+# Add DOTS binaries to the $PATH
+PATH=$DOTS_PATH:$PATH
