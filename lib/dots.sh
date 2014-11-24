3477c524f39bbd165aefc02a3ba0d012af348913
Tom Scott <tscott@telvue.com>
2014-11-24 11:15:38 -0500

    Don't fuck with the path in dots.sh

--- lib/dots.sh
+++ lib/dots.sh
@@ -1,9 +1,6 @@
 # add a function path
 fpath=($ZSH/lib/dots/functions $ZSH/lib/dots/completion $fpath)
 
-# static binary path
-export DOTS_PATH=$HOME/.dots/bin
-
 # Load and run compinit
 autoload -U compinit
 compinit -i
@@ -12,6 +9,3 @@ compinit -i
 source "$ZSH/lib/dots/functions.zsh"
 source "$ZSH/lib/dots/aliases.zsh"
 source "$ZSH/lib/dots/plugins.zsh"
-
-# Add DOTS binaries to the $PATH
-PATH=$DOTS_PATH:$PATH
