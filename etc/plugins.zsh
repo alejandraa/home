b866b2f67d06084c609269e3555b8dd82d7b4395
Tom Scott <tscott@telvue.com>
2014-11-26 18:32:13 -0500

    Install plugins zsh-syntax-highlighting and autoenv

---
+++ etc/plugins.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+#
+# Plugin manifest file, started off with a few plugins that most people
+# find useful.
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle kennethreitz/autoenv
