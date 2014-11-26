741a41297d94db4df8b380b37e7bb12c421c6520
Tom Scott <tscott@telvue.com>
2014-11-26 01:35:30 -0500

    Remove prompt config from prompt.zsh

--- etc/profile.d/prompt.zsh
+++ etc/profile.d/prompt.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-# A more basic version of my promptstring for non-interactive shells.
-PROMPT="♬  "
 
 # Use powerline for the prompt
 source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
