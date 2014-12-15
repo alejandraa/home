e195aceaafc7ab983bac374db4442d373ec7aa39
Tom Scott <tscott@telvue.com>
2014-12-15 12:17:13 -0500

    Add berkshelf aliases

--- etc/profile.d/chef.zsh
+++ etc/profile.d/chef.zsh
@@ -1,3 +1,10 @@
+#!zsh
+
 # Use vagrant-digitalocean
 export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
+# Set up knife aliases
+alias ku='knife upload'
+alias bki='berks install'
+alias bku='berks upload'
+alias bkc='berks cookbook'
