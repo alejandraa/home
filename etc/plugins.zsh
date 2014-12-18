11244477f20136ee4a38159dde45252d2a82d57c
Tom Scott <tscott@telvue.com>
2014-12-18 17:48:33 -0500

    Use the correct antigen path

--- etc/plugins.zsh
+++ etc/plugins.zsh
@@ -3,5 +3,7 @@
 # Plugin manifest file, started off with a few plugins that most people
 # find useful.
 
+source /usr/local/share/antigen.zsh
+
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle kennethreitz/autoenv
