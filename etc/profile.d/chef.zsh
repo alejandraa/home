9b795fade08c3c95a090102a26b3cd78637629f3
Tom Scott <tscott@telvue.com>
2014-12-31 02:37:33 -0500

    First flight

--- etc/profile.d/chef.zsh
+++ etc/profile.d/chef.zsh
@@ -1,8 +1,5 @@
 #!zsh
 
-# Use vagrant-digitalocean
-export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
-
 # Set up knife aliases
 alias ku='knife upload'
 alias bki='berks install'
