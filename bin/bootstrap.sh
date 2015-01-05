73970133120d984a276d950c7331e94ca6ff7371
Tom Scott <tscott@telvue.com>
2015-01-05 01:33:29 -0500

    Add bootstrap script

---
+++ bin/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# A bootstrap script for installing Homebrew, Homer, and my home
+# directory. Meant to be run directly in the shell as pretty much the
+# first command you run on a new machine install.
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install homer
+homer init -c git@github.com:tubbo/home.git
+brew flight install
