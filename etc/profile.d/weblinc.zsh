177aa083a5ec6a4690c435d1357408c5c8d826c9
Tom Scott <tscott@weblinc.com>
2015-04-30 11:59:16 -0400

    Add alias to restart demo app

--- etc/profile.d/weblinc.zsh
+++ etc/profile.d/weblinc.zsh
@@ -1,3 +1,4 @@
 #!zsh
 
 alias weblinc="chruby-exec 2.2.0 -- GEMS_PATH='~/Code' /usr/bin/rails new $1 --template=~/Code/weblinc/docs/guides/source/app_template.rb --skip-active-record --no-rc"
+alias rsd='pw restart demo'
