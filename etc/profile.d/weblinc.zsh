42d3de4be5a9b7239d3b87b44adfa17dc075cb00
Tom Scott <tscott@weblinc.com>
2015-05-04 10:39:03 -0400

    Add alias to turn Spring off

--- etc/profile.d/weblinc.zsh
+++ etc/profile.d/weblinc.zsh
@@ -2,3 +2,4 @@
 
 alias weblinc="chruby-exec 2.2.0 -- GEMS_PATH='~/Code' /usr/bin/rails new $1 --template=~/Code/weblinc/docs/guides/source/app_template.rb --skip-active-record --no-rc"
 alias rsd='pw restart demo'
+alias ss='spring stop'
