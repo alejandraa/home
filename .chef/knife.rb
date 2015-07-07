667945cce2cb61d55fbf2a944f025bb124d41bee
Tom Scott <tscott@weblinc.com>
2015-07-07 13:52:02 -0400

    Add global Chef configuration for waxpoetic server

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'tscott'
+client_key               '/Users/tscott/.chef/tubbo.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/tscott/.chef/waxpoetic-validator.pem'
+syntax_check_cache_path  '/Users/tscott/.chef/syntax_check_cache'
+#chef_server_url          'https://coda.local:443'
